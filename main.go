a4a28dd1cf97aefd77bccc74062e4fd8a0daecdb
prime-infinity <soe.a.prime@gmail.com>
2021-10-21 21:23:11 +0100

    still in arrays

--- main.go
+++ main.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 
-	var ages = [3]int{20, 25, 30}
+	var ages = [...]int{20, 25, 30}
 	fmt.Println(ages)
 
-	names := [4]string{"yoshi", "mario", "peach", "bowser"}
+	names := [...]string{"yoshi", "mario", "peach", "bowser"}
 	fmt.Println(names)
 
 }
