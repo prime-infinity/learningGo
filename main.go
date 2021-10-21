8324bae7272cc21b2400dcb4d6ec1c6395cb7945
prime-infinity <soe.a.prime@gmail.com>
2021-10-21 21:57:25 +0100

    slices done

--- main.go
+++ main.go
@@ -4,11 +4,25 @@ import "fmt"
 
 func main() {
 
-	a := [...]int{1, 2, 3}
-	b := &a //copying arrays will literally copy the array
+	//slices
+	var scores = []int{100, 42, 21, 43}
 
-	b[1] = 5
-	fmt.Println(a)
-	fmt.Println(b)
+	//fmt.Println(scores)
+
+	scores[2] = 332
+
+	//fmt.Println(scores)
+
+	scores = append(scores, 85) //append something to the end of a slice
+
+	//fmt.Println(scores)
+
+	//slice ranges
+	rangeOne := scores[1:3]  //range of 1 to 2
+	rangeTwo := scores[2:]   //from 2 to end
+	rangeThree := scores[:3] //from start to before 3
+	fmt.Println(rangeOne)
+	fmt.Println(rangeTwo)
+	fmt.Println(rangeThree)
 
 }
