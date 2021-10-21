ca93f38cf48018b66c05f6e57113966a547d56b6
prime-infinity <soe.a.prime@gmail.com>
2021-10-21 02:02:02 +0100

    done strings variables

--- main.go
+++ main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("i am alive")
+
+	//strings
+	var stringOne string = "this is a string" //strings in go are double quoted
+	var stringTwo = "this is another string"  //here, go sets type automatically
+	var stringThree string                    //declared, but not used now
+
+	fmt.Println(stringOne)
+	fmt.Println(stringTwo, stringThree)
+
+	stringThree = "i will do your biding master" //values can be reassigned but not types
+
+	fmt.Println(stringThree)
+
+	stringFour := "another servant of the force" //same as other methods, but cannot be used outside a function
+	stringFour = "he will serve well"
+	fmt.Println(stringFour)
+
 }
