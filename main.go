c7b1b6e37ce7f81d34b3f60c1e6564d61fd6fab9
prime-infinity <soe.a.prime@gmail.com>
2021-10-21 16:57:45 +0100

    declaring multiple variables at package level

--- main.go
+++ main.go
@@ -1,41 +1,16 @@
 package main
 
+import "fmt"
+
+var (
+	actorName    string = "Elizabeth sladen"
+	companion    string = "Sarah jane smith"
+	doctorNumber int    = 3
+	season       int    = 11
+)
+
 func main() {
 
-	//strings
-	/*var stringOne string = "this is a string" //strings in go are double quoted
-	var stringTwo = "this is another string"  //here, go sets type automatically
-	var stringThree string                    //declared, but not used now
-
-	fmt.Println(stringOne)
-	fmt.Println(stringTwo, stringThree)
-
-	stringThree = "i will do your biding master" //values can be reassigned but not types
-
-	fmt.Println(stringThree)
-
-	stringFour := "another servant of the force" //same as other methods, but cannot be used outside a function
-	stringFour = "he will serve well"
-	fmt.Println(stringFour)*/
-
-	//ints
-	/*var numOne int = 20
-	var numTwo = 30
-	numThree := 40
-
-	fmt.Println(numOne, numTwo, numThree)*/
-
-	//floats
-	/*var floatOne float32 = 34.53
-	var floatTwo float64 = 332323.423
-	floatDefault := 2.3
-
-	fmt.Println(floatOne, floatTwo, floatDefault)*/
-
-	/*var i int
-	i = 42*/
-	//var j int = 27
-	//k := 99
-	//fmt.Printf("%v, %T", j, j)
+	fmt.Printf("%v, %T", actorName, actorName)
 
 }
