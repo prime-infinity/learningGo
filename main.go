905cf5d9313c164973ab3d8db210aa3c2b8234de
prime-infinity <soe.a.prime@gmail.com>
2021-10-21 21:42:12 +0100

    in arrays, pointing one to another

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 
 	a := [...]int{1, 2, 3}
-	b := a //copying arrays will literally copy the array
+	b := &a //copying arrays will literally copy the array
 
 	b[1] = 5
 	fmt.Println(a)
