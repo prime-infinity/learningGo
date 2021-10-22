7ce3027ad4f2cf2ff3a9197e59b0e73ef9a22c74
prime-infinity <soe.a.prime@gmail.com>
2021-10-23 00:25:17 +0100

    maps

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main() {
 
+	//maps are like objects in other languages, but like really strict objects
+
 	statePopulations := map[string]int{ //key value pairs must be of the same type
 		"califonia":    23423123,
 		"texas":        2324232,
@@ -14,11 +16,19 @@ func main() {
 		"ohio":         12323232,
 	}
 
-	//fmt.Println(statePopulations)
+	statePopulations["califonia"] = 000000     //updating a value
+	fmt.Println(statePopulations["califonia"]) //getting a single value from a key
 
-	for index, value := range statePopulations {
+	delete(statePopulations, "califonia") //deleting from map
+
+	_, ok := statePopulations["new"] //cheching for the existence of a key, value pair
+	fmt.Println(ok)
+
+	//note, return order of a map is not guranteed
+
+	/*for index, value := range statePopulations {
 		fmt.Println(index)
 		fmt.Println(value)
-	}
+	}*/
 
 }
