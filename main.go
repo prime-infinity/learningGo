439f59823d81f99829eec06b5f2afdb5bca91f0d
prime-infinity <soe.a.prime@gmail.com>
2021-10-22 23:45:21 +0100

    referencing and dereferencing pointers

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import "fmt"
+
+func updateName(x string) {
+	x = "wedge"
+}
+
 func main() {
+
+	name := "tifa"
+
+	updateName(name)
+
+	//fmt.Println("memory address of name is:", &name) //referencing pointers
+
+	m := &name //referencing pointers
+	fmt.Println("memory address ", m)
+
+	fmt.Println("value at memory address is :", *m)
+
+	//fmt.Println(name)
 
 }
