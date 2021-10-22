5b9dede03a3fe6b1bbfec309278320c14128be63
prime-infinity <soe.a.prime@gmail.com>
2021-10-22 10:28:12 +0100

    fresh

--- main.go
+++ main.go
@@ -1,23 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-
-	names := []string{"mario", "luigi", "yoshi", "peach", "bowser"}
-
-	for index, value := range names {
-		if index == 1 {
-			fmt.Println("continuing at pos", index)
-			continue //break out of current iteration of loop
-		}
-
-		if index > 2 {
-			fmt.Println("breaking at pos", index)
-			break //breaks out of the loop entirely
-		}
-
-		fmt.Printf("the value at pos %v is %v \n", index, value)
-	}
 
 }
