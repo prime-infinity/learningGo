653b7d050cc294614d853a2451f5e0812068ae75
prime-infinity <soe.a.prime@gmail.com>
2021-10-22 23:15:15 +0100

    fresh

--- greeings.go
+++ greeings.go
@@ -1,13 +1,1 @@
 package main
-
-import "fmt"
-
-var points = []int{20, 30, 40, 100, 34}
-
-func sayHello(n string) {
-	fmt.Println("hello", n)
-}
-
-func showScore() {
-	fmt.Println("you scored :", score)
-}
--- main.go
+++ main.go
@@ -1,17 +1,5 @@
 package main
 
-import "fmt"
-
-var score = 99.5 //to export, remember, cannot use := at package level
-
 func main() {
-
-	sayHello("mario")
-
-	for _, v := range points {
-		fmt.Println(v)
-	}
-
-	showScore()
 
 }
