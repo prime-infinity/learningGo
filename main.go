c36f16d69e37627d6bed97981302f9c9ba1465aa
prime-infinity <soe.a.prime@gmail.com>
2021-10-23 00:04:50 +0100

    pointers

--- main.go
+++ main.go
@@ -2,23 +2,27 @@ package main
 
 import "fmt"
 
-func updateName(x string) {
-	x = "wedge"
+func updateName(x *string) {
+	*x = "wedge"
 }
 
 func main() {
 
 	name := "tifa"
 
-	updateName(name)
+	//updateName(name)
 
 	//fmt.Println("memory address of name is:", &name) //referencing pointers
 
 	m := &name //referencing pointers
-	fmt.Println("memory address ", m)
 
-	fmt.Println("value at memory address is :", *m)
+	//fmt.Println("memory address ", m)
 
-	//fmt.Println(name)
+	//fmt.Println("value at memory address is :", *m)
+
+	fmt.Println(name)
+	updateName(m)
+
+	fmt.Println(name)
 
 }
