46b42a262d33244489119f080a5cf70e790e2ffa
prime-infinity <soe.a.prime@gmail.com>
2021-10-23 23:08:22 +0100

    switch statements

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 )
 
+//remenber that user inputs are always stings
 func getInput(prompt string, r *bufio.Reader) (string, error) {
 	fmt.Print(prompt)
 	prompt, err := r.ReadString('\n')
@@ -30,7 +31,21 @@ func promptOptions(b bill) {
 	reader := bufio.NewReader(os.Stdin)
 
 	opt, _ := getInput("choose option (a - add item, s - save bill, t - add tip): ", reader)
-	fmt.Println(opt)
+
+	switch opt {
+	case "a":
+		name, _ := getInput("item name:", reader)
+		price, _ := getInput("item price:", reader)
+		fmt.Println(name, price)
+	case "t":
+		tip, _ := getInput("enter tip amount ($): ", reader)
+		fmt.Println(tip)
+	case "s":
+		fmt.Println("you choose s")
+	default:
+		fmt.Println("that was not a valid option...")
+		promptOptions(b)
+	}
 }
 
 func main() {
