6e0960a5dafc42563c4297e32da03102a6d4de8c
prime-infinity <soe.a.prime@gmail.com>
2021-10-23 11:04:53 +0100

    creating structs like oop

--- bill.go
+++ bill.go
@@ -1,15 +1,25 @@
 package main
 
+//note that the "biil" is now a type onits own and can be used as return in functions
 type bill struct {
-	name  string
-	items map[string]float64
-	tip   float64
-	good  bool
+	name   string
+	items  map[string]float64
+	tip    float64
+	status bool
 }
 
 //when defining a struct, the name and the type is needed
 
 //make new bills function
 func newBill(name string) bill {
+
+	b := bill{
+		name:   name,
+		items:  map[string]float64{},
+		tip:    0,
+		status: true,
+	}
+
+	return b
 
 }
--- main.go
+++ main.go
@@ -3,26 +3,13 @@ package main
 import "fmt"
 
 //a simple struct can mix any type of data together
-type testStruct struct {
-	number     int
-	actorName  string
-	companions []string
-}
 
 func main() {
 
 	//structs are like advanced maps
 	//structs are custom types
 
-	cStruct := testStruct{
-		number:    3,
-		actorName: "mayiam",
-		companions: []string{
-			"this", "that", "tes",
-		},
-	}
-
-	fmt.Println(cStruct)
-	fmt.Println(cStruct.companions)
+	myBill := newBill("osamede's bill")
+	fmt.Println(myBill)
 
 }
